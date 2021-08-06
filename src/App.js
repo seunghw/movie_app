7534b6ec675f8d8fbf3cbf23bd62fc4e4dee0016
Seunghwan <ysp1307@naver.com>
2021-08-06 22:44:09 +0900

    TMDB API를 이용하여 데이터 state화 및 mainImage 삽입 완료

--- src/App.js
+++ src/App.js
@@ -1,7 +1,48 @@
-import React from "react";
+//eslint-disable-next-line
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { API_URL, API_KEY, IMAGE_BASE_URL } from "./Config";
+import MainImage from "./MainImage";
 
 function App() {
-  return <div className="App"></div>;
+  const [Movies, setMovies] = useState([]);
+  const [MainMovieImage, setMainMovieImage] = useState(null);
+
+  useEffect(() => {
+    const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=ko-US&page=1`;
+
+    fetch(endpoint)
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+        setMovies([response.results]);
+        setMainMovieImage(response.results[2]);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <div style={{ width: "100%", margin: "0" }}>
+        {MainMovieImage && (
+          <MainImage
+            image={`${IMAGE_BASE_URL}w1280${MainMovieImage.backdrop_path}`}
+            title={MainMovieImage.original_title}
+            text={MainMovieImage.overview}
+          />
+        )}
+        <div style={{ width: "85%", margin: "1rem auto" }}>
+          <h2>Movies by lastest</h2>
+          <hr />
+
+          {/* Movie Grid Cards  */}
+        </div>
+
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <button> Load More</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Config.js
@@ -0,0 +1,3 @@
+export const API_URL = "https://api.themoviedb.org/3/";
+export const IMAGE_BASE_URL = "http://image.tmdb.org/t/p/";
+export const API_KEY = "21c25830deb75fca11ba908ab3bb3493";
