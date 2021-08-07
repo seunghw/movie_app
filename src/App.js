1db2243d3ca980df875c6814392f38e6bb516f74
Seunghwan <ysp1307@naver.com>
2021-08-07 23:00:17 +0900

    tmdb api 메인페이지 css적용 완료

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 //eslint-disable-next-line
 import React, { useEffect, useState } from "react";
-import axios from "axios";
 import { API_URL, API_KEY, IMAGE_BASE_URL } from "./Config";
 import MainImage from "./MainImage";
+import GridCards from "./GridCards";
+import { Row } from "antd";
+import Test from "./Test";
 
 function App() {
   const [Movies, setMovies] = useState([]);
@@ -14,15 +16,29 @@ function App() {
     fetch(endpoint)
       .then((response) => response.json())
       .then((response) => {
-        console.log(response);
-        setMovies([response.results]);
-        setMainMovieImage(response.results[2]);
+        console.log(response.results);
+        setMovies([...Movies, ...response.results]);
+        setMainMovieImage(response.results[1]);
       });
   }, []);
 
   return (
     <div className="App">
       <div style={{ width: "100%", margin: "0" }}>
+        <div>하이</div>
+        {Movies && (
+          <React.Fragment>
+            <Test
+              image={
+                Movies.poster_path
+                  ? `${IMAGE_BASE_URL}w500${Movies.poster_path}`
+                  : null
+              }
+              movieId={Movies.id}
+              movieName={Movies.original_title}
+            />
+          </React.Fragment>
+        )}
         {MainMovieImage && (
           <MainImage
             image={`${IMAGE_BASE_URL}w1280${MainMovieImage.backdrop_path}`}
@@ -34,7 +50,22 @@ function App() {
           <h2>Movies by lastest</h2>
           <hr />
 
-          {/* Movie Grid Cards  */}
+          <Row gutter={[16, 16]}>
+            {Movies &&
+              Movies.map((movie, index) => (
+                <React.Fragment key={index}>
+                  <GridCards
+                    image={
+                      movie.poster_path
+                        ? `${IMAGE_BASE_URL}w500${movie.poster_path}`
+                        : null
+                    }
+                    movieId={movie.id}
+                    movieName={movie.original_title}
+                  />
+                </React.Fragment>
+              ))}
+          </Row>
         </div>
 
         <div style={{ display: "flex", justifyContent: "center" }}>
